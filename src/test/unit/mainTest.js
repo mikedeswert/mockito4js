e1f4636776b3e695c160b194acddcc7b224462ca
mdeswer1 <mike.de.swert@staff.telenet.be>
2015-02-22 13:07:14 +0100

    added argument support + added mockito4js.any()

--- src/test/unit/mainTest.js
+++ src/test/unit/mainTest.js
@@ -57,5 +57,152 @@ describe('mockito4js', function () {
 
             mockito4js.verify(verifierSpy, mockito4js.once()).verify('functionOne', 1);
         });
+
+        it('should not throw an error if arguments of function call are of given type when any is used', function() {
+            var verifierSpy = mockito4js.spy(mockito4js.once());
+            objectSpy.functionOne('argumentOne');
+
+            mockito4js.verify(objectSpy, verifierSpy).functionOne(mockito4js.any('string'));
+        });
+
+        it('should throw an error if arguments of function call are of given type when any is used', function() {
+            var verifier = mockito4js.once();
+            objectSpy.functionOne(0);
+
+            expect(function() {
+                mockito4js.verify(objectSpy, verifier).functionOne(mockito4js.any('string'))
+            }).toThrow(new Error('Number of invocations of "functionOne" does not match the expected amount of ' + verifier.invocationCount + '.' +
+            ' Actual number of invocations is 0'));
+        });
+    });
+
+    describe('any', function() {
+        it('should return an Any object with a type and match function', function() {
+            var actual = mockito4js.any(Object);
+
+            expect(actual.type).toBe(Object);
+            expect(typeof actual.matches).toBe('function');
+        });
+
+        describe('matches', function() {
+            it('should return true if given argument is Object and type of any is Object', function() {
+                var any = mockito4js.any(Object);
+                var argument = new Object();
+
+                var actual = any.matches(argument);
+
+                expect(actual).toBe(true);
+            });
+
+            it('should return true if given argument is Object and type of any is Object literal', function() {
+                var any = mockito4js.any(Object);
+                var argument = {};
+
+                var actual = any.matches(argument);
+
+                expect(actual).toBe(true);
+            });
+
+            it('should return true if given argument is Array and type of any is Array', function() {
+                var any = mockito4js.any(Array);
+                var argument = new Array();
+
+                var actual = any.matches(argument);
+
+                expect(actual).toBe(true);
+            });
+
+            it('should return true if given argument is Array and type of any is Array literal', function() {
+                var any = mockito4js.any(Array);
+                var argument = [];
+
+                var actual = any.matches(argument);
+
+                expect(actual).toBe(true);
+            });
+
+            it('should return true if given argument is Function and type of any is Function', function() {
+                var any = mockito4js.any(Function);
+                var argument = function() {};
+
+                var actual = any.matches(argument);
+
+                expect(actual).toBe(true);
+            });
+
+            it('should return true if given argument is String and type of any is String', function() {
+                var any = mockito4js.any(String);
+                var argument = new String('argument');
+
+                var actual = any.matches(argument);
+
+                expect(actual).toBe(true);
+            });
+
+            it('should return true if given argument is String and type of any is String literal', function() {
+                var any = mockito4js.any('string');
+                var argument = 'argument';
+
+                var actual = any.matches(argument);
+
+                expect(actual).toBe(true);
+            });
+
+            it('should return true if given argument is Boolean and type of any is Boolean', function() {
+                var any = mockito4js.any(Boolean);
+                var argument = new Boolean(true);
+
+                var actual = any.matches(argument);
+
+                expect(actual).toBe(true);
+            });
+
+            it('should return true if given argument is Boolean and type of any is Boolean literal', function() {
+                var any = mockito4js.any('boolean');
+                var argument = true;
+
+                var actual = any.matches(argument);
+
+                expect(actual).toBe(true);
+            });
+
+            it('should return true if given argument is Number and type of any is Number', function() {
+                var any = mockito4js.any(Number);
+                var argument = new Number(0);
+
+                var actual = any.matches(argument);
+
+                expect(actual).toBe(true);
+            });
+
+            it('should return true if given argument is Number and type of any is Number literal', function() {
+                var any = mockito4js.any('number');
+                var argument = 0;
+
+                var actual = any.matches(argument);
+
+                expect(actual).toBe(true);
+            });
+
+            it('should return true if given argument is of custom type and type of any is same custom type', function() {
+                function CustomType() {}
+
+                var any = mockito4js.any(CustomType);
+                var argument = new CustomType();
+
+                var actual = any.matches(argument);
+
+                expect(actual).toBe(true);
+            });
+
+            it('should return false if given argument is not an instance of given type', function() {
+                var any = mockito4js.any(Function);
+                var argument = {};
+
+                var actual = any.matches(argument);
+
+                expect(actual).toBe(false);
+            });
+        });
     });
 });
